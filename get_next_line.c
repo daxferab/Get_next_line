3587d4afbd2912b40165b38460f9ea34de73e164
Dax Fernandez-Abascal Fernandez <daxferna@c1r12s6.42madrid.com>
2024-07-16 23:11:44 +0200

    updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: daxferab <daxferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 12:43:01 by daxferab          #+#    #+#             */
-/*   Updated: 2024/07/16 22:23:49 by daxferab         ###   ########.fr       */
+/*   Updated: 2024/07/16 23:11:02 by daxferab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@ char	*get_next_line(int fd)
 	static char	*remain;
 	char		*buffer;
 
+	if (remain && remain != "")
+	{
+		while (remain != 0)
+		{
+			//concatenar el caracter al buffer
+		}
+	}
 	read(fd, buffer, BUFFER_SIZE);
 	return (buffer);
 }
